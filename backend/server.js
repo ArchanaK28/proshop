866dd0b93bcdf6e74fac1e5ca436eca81fffc091
ArchanaK <archana.ka@ibm.com>
2024-09-29 21:35:31 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 
@@ -20,13 +21,15 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use('/api/orders', orderRoutes);
+
+app.get('api/config/paypal', (req, res) => res.send({ clientId:process.env.PAYPAL_CLIENT_ID}));
 
 app.use(notFound);
 app.use(errorHandler);
